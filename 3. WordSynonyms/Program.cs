48d564913e1b2537359ac34e412b55c69530ce1a
Albert Ivanov <albertivanov187@gmail.com>
2024-12-22 00:12:14 +0200

    Update

---
+++ 3. WordSynonyms/Program.cs
@@ -0,0 +1,14 @@
+﻿Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < number; i++)
+{
+	string word = Console.ReadLine();
+	string synonym = Console.ReadLine();
+
+	if (words.ContainsKey(word))
+	{
+		
+	}
+}
