249af0bfbc3d99d3578be0cdb2400616c1beb3ff
Albert Ivanov <albertivanov187@gmail.com>
2024-12-21 17:33:41 +0200

    Add project files.

---
+++ 01. CountRealNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._CountRealNumbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+			SortedDictionary<double, int> dictionary = new SortedDictionary<double, int>();
+
+			for (int i = 0; i < numbers.Count; i++)
+			{
+				if (!dictionary.ContainsKey(numbers[i]))
+				{
+					dictionary.Add(numbers[i], 0);
+				}
+
+				dictionary[numbers[i]]++;
+			}
+
+			foreach (var number in dictionary)
+			{
+				Console.WriteLine($"{number.Key} -> {number.Value}");
+			}
+		}
+	}
+}
