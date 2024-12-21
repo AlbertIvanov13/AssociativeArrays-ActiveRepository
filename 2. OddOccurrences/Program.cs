b793415bc8a572402ac9093526fd07d9fe264bef
Albert Ivanov <albertivanov187@gmail.com>
2024-12-22 00:00:32 +0200

    Update

---
+++ 2. OddOccurrences/Program.cs
@@ -0,0 +1,24 @@
+﻿List<string> words = Console.ReadLine().Split().ToList();
+
+Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+
+foreach (var word in words)
+{
+	string lowerCaseWord = word.ToLower();
+	if (keyValuePairs.ContainsKey(lowerCaseWord))
+	{
+		keyValuePairs[lowerCaseWord]++;
+	}
+	else
+	{
+		keyValuePairs.Add(lowerCaseWord, 1);
+	}
+}
+
+foreach (var element in keyValuePairs)
+{
+	if (element.Value % 2 != 0)
+	{
+        Console.Write(element.Key + " ");
+	}
+}
