962b8270939b788f134f509146314794791383f4
Albert Ivanov <albertivanov187@gmail.com>
2025-01-04 21:36:44 +0200

    Update 100/100

--- 5. CountCharsInAString/Program.cs
+++ 5. CountCharsInAString/Program.cs
@@ -5,21 +5,23 @@ Dictionary<char, int> dictionary = new Dictionary<char, int>();
 
 var withoutSpace = text.Where(t => t != ' ').ToList();
 
+List<char> letters = new List<char>();
 int counter = 0;
 for (int i = 0; i < withoutSpace.Count; i++)
 {
 	char symbol = withoutSpace[i];
+	letters.Add(symbol);
+
 	if (!dictionary.ContainsKey(symbol))
 	{
-		if (counter == 0)
+		counter = 0;
+		counter++;
+		dictionary.Add(symbol, counter);
+		var letterss = text.Where(t => t == symbol).ToList();
+		foreach (var item in letterss)
 		{
-			counter++;
-			dictionary.Add(symbol, counter);
+			Console.WriteLine($"{item} -> {letterss.Count}");
+			break;
 		}
 	}
-}
-
-foreach (var symbol in dictionary)
-{
-    Console.WriteLine($"{symbol.Key} -> {symbol.Value}");
 }
