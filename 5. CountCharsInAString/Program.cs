58131be0112405a93c4c253ca2465b5a1fb52aab
Albert Ivanov <albertivanov187@gmail.com>
2025-01-04 21:46:02 +0200

    100/100 with dictionary

--- 5. CountCharsInAString/Program.cs
+++ 5. CountCharsInAString/Program.cs
@@ -6,7 +6,6 @@ Dictionary<char, int> dictionary = new Dictionary<char, int>();
 var withoutSpace = text.Where(t => t != ' ').ToList();
 
 List<char> letters = new List<char>();
-int counter = 0;
 for (int i = 0; i < withoutSpace.Count; i++)
 {
 	char symbol = withoutSpace[i];
@@ -14,14 +13,12 @@ for (int i = 0; i < withoutSpace.Count; i++)
 
 	if (!dictionary.ContainsKey(symbol))
 	{
-		counter = 0;
-		counter++;
-		dictionary.Add(symbol, counter);
 		var letterss = text.Where(t => t == symbol).ToList();
-		foreach (var item in letterss)
-		{
-			Console.WriteLine($"{item} -> {letterss.Count}");
-			break;
-		}
+		dictionary.Add(symbol, letterss.Count);
 	}
+}
+
+foreach (var item in dictionary)
+{
+	Console.WriteLine($"{item.Key} -> {item.Value}");
 }
