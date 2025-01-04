d2666f191d4bac5617d9906be8c878cabb6a5fda
Albert Ivanov <albertivanov187@gmail.com>
2025-01-04 22:07:23 +0200

    100/100 :D

---
+++ 6. AMinerTask/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Dictionary<string, int> dictionary = new Dictionary<string, int>();
+ 
+while (true)
+{
+	string resource = Console.ReadLine();
+
+	if (resource == "stop")
+	{
+		break;
+	}
+
+	int quantity = int.Parse(Console.ReadLine());
+
+	if (dictionary.ContainsKey(resource))
+	{
+		dictionary[resource] += quantity;
+	}
+	else
+	{
+		dictionary.Add(resource, quantity);
+	}
+}
+
+foreach (var item in dictionary)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
