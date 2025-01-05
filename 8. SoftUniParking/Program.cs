03a29d8243af687be8469e62ab7042818cf18fd7
Albert Ivanov <albertivanov187@gmail.com>
2025-01-06 00:01:28 +0200

    100/100 :D

--- 8. SoftUniParking/Program.cs
+++ 8. SoftUniParking/Program.cs
@@ -16,12 +16,35 @@
 				{
 					string name = commands[1];
 					string license = commands[2];
-
+					if (cars.ContainsKey(name))
+					{
+						Console.WriteLine($"ERROR: already registered with plate number {license}");
+					}
+					else
+					{
+						var car = new Car(name, license);
+						cars.Add(name, car);
+						Console.WriteLine($"{name} registered {license} successfully");
+					}
 				}
 				else if (decision == "unregister")
 				{
 					string name = commands[1];
+					if (!cars.ContainsKey(name))
+					{
+                        Console.WriteLine($"ERROR: user {name} not found");
+					}
+					else
+					{
+						cars.Remove(name);
+                        Console.WriteLine($"{name} unregistered successfully");
+					}
 				}
+			}
+
+			foreach (Car item in cars.Values)
+			{
+                Console.WriteLine($"{item.Username} => {item.LicensePlateNumber}");
 			}
 		}
 
@@ -30,6 +53,7 @@
             public string Username { get; set; }
             public string LicensePlateNumber { get; set; }
 
+			public List<string> Cars { get; set; }
             public Car(string username, string licensePlateNumber)
             {
                 Username = username;
