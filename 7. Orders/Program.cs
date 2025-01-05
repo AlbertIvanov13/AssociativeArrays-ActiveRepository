f75281af59972b7e1e167712012f5d0a99b76b0e
Albert Ivanov <albertivanov187@gmail.com>
2025-01-05 23:06:41 +0200

    Update

--- 7. Orders/Program.cs
+++ 7. Orders/Program.cs
@@ -6,7 +6,30 @@ namespace _7._Orders
 	{
 		static void Main(string[] args)
 		{
-			
+			Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+			while (true)
+			{
+				string[] commands = Console.ReadLine().Split();
+				string name = commands[0];
+				double price = double.Parse(commands[1]);
+				int quantity = int.Parse(commands[2]);
+
+				if (name == "buy")
+				{
+					break;
+				}
+
+				if (products.ContainsKey(name))
+				{
+
+				}
+			}
+
+			foreach (string name in products.Keys)
+			{
+
+			}
 		}
 
 		public class Product
@@ -17,9 +40,16 @@ namespace _7._Orders
 
             public int Quantity { get; set; }
 
-			public override string ToString()
+            public Product(string name, double price, int quantity)
+            {
+                Name = name;
+				Price = price;
+				Quantity = quantity;
+            }
+
+            public override string ToString()
 			{
-				return $"{Name} -> {Quantity}";
+				return $"{Name} -> {Quantity * Price}";
 			}
 		}
 	}
