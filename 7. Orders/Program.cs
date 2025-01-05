ff138c37bb0dae02f884d075e2f0a20f0396c312
Albert Ivanov <albertivanov187@gmail.com>
2025-01-05 23:03:02 +0200

    I want to make it using a class

--- 7. Orders/Program.cs
+++ 7. Orders/Program.cs
@@ -1,57 +1,12 @@
-﻿namespace _7._Orders
+﻿using System.Security.AccessControl;
+
+namespace _7._Orders
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Dictionary<string, double> dictionary = new Dictionary<string, double>();
-
-			double price = 0;
-			while (true)
-			{
-				string[] input = Console.ReadLine().Split().ToArray();
-				string name = input[0];
-				if (name == "buy")
-				{
-					break;
-				}
-				int quantity = int.Parse(input[2]);
-				//if (dictionary.ContainsKey(name))
-				//{
-				//	foreach (var item in dictionary)
-				//	{
-				//		if (item.Key == name)
-				//		{
-				//			dictionary[name] /= item.Value;
-				//		}
-				//	}
-				//}
-				price = double.Parse(input[1]);
-
-				if (dictionary.ContainsKey(name))
-				{
-					foreach (var item in dictionary)
-					{
-						if (item.Key == name)
-						{
-							dictionary[name] += quantity;
-							double newPrice = price;
-							dictionary[name] *= price;
-						}
-					}
-				}
-
-				if (!dictionary.ContainsKey(name))
-				{
-					dictionary.Add(name, quantity);
-					dictionary[name] = price * quantity;
-				}
-			}
-
-			foreach (var item in dictionary)
-			{
-				Console.WriteLine($"{item.Key} -> {item.Value:f2}");
-			}
+			
 		}
 
 		public class Product
